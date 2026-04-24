3116eb5b47891e1c8d563d684dff8023e15d3549
Zoy0707 <127084324+Zoy0707@users.noreply.github.com>
2026-04-24 16:24:24 +1000

    update google verification2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  verification: {
-    google: '<meta name="google-site-verification" content="of8U4HofWREi0Im5SZOhD30OLvHeIcDGYCvNBLba3KI" />'
-  },
-  metadataBase: new URL("https://YOUR-PROJECT.vercel.app"),
+  metadataBase: new URL("https://competition2026.innovateconnectalliance.org"),
   title: {
     default: "UoM Engineering & IT Innovation Competition 2026",
     template: "%s | ICA",
@@ -22,11 +19,14 @@ export const metadata: Metadata = {
     "ICA",
     "Innovate Connect Alliance",
   ],
+  verification: {
+    google: "of8U4HofWREi0Im5SZOhD300LvHeIcDGYCvNBLba3KI",
+  },
   openGraph: {
     title: "UoM Engineering & IT Innovation Competition 2026",
     description:
       "A cross-disciplinary innovation challenge bringing together students, recent graduates, and early-stage builders to turn bold ideas into practical solutions.",
-    url: "https://YOUR-PROJECT.vercel.app",
+    url: "https://competition2026.innovateconnectalliance.org",
     siteName: "Innovate Connect Alliance",
     locale: "en_AU",
     type: "website",
